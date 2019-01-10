366bccb2ea94ca830c46c9b10436f1bb288fd629
IGNovikov <IGNovikov@esphere.ru>
2019-01-10 11:30:04 +0300

    add class NotString

---
+++ src/main/java/Warmup1/NotString.java
@@ -0,0 +1,17 @@
+package Warmup1;
+
+/*Given a string, return a new string where "not " has been added to the front. However, if the string already begins with "not", return the string unchanged. Note: use .equals() to compare 2 strings.*/
+
+public class NotString {
+
+    public static void main(String[] args) {
+        System.out.println(notString("candy"));
+        System.out.println(notString("x"));
+        System.out.println(notString("not bad"));
+    }
+
+    private static String notString (String str) {
+        return str.startsWith("not")?str:"not " +str;
+    }
+
+}
