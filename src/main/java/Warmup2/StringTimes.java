db9beda0a16c7de90e2bc2fec17157757dbf16e3
IGNovikov <IGNovikov@esphere.ru>
2019-01-10 12:04:42 +0300

    Create branch Warmup-2 add package Warmup2 add class StringTimes

---
+++ src/main/java/Warmup2/StringTimes.java
@@ -0,0 +1,21 @@
+package Warmup2;
+
+/*Given a string and a non-negative int n, return a larger string that is n copies of the original string.*/
+
+public class StringTimes {
+
+    public static void main(String[] args) {
+        System.out.println(stringTimes("Hi", 1));
+        System.out.println(stringTimes("Hello", 4));
+        System.out.println(stringTimes("Code", 7));
+    }
+
+    private static String stringTimes(String str, int n) {
+        StringBuilder temp = new StringBuilder();
+        for (int i = 0; i<n; i++) {
+            temp.append(str);
+        }
+        return temp.toString();
+    }
+
+}
