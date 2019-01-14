ebc5dba39384b496f010fc8abece8a9045e872a8
IGNovikov <IGNovikov@esphere.ru>
2019-01-14 09:20:59 +0300

    add description to class Close10

--- src/main/java/Warmup1/Close10.java
+++ src/main/java/Warmup1/Close10.java
@@ -1,5 +1,7 @@
 package Warmup1;
 
+/*Given 2 int values, return whichever value is nearest to the value 10, or return 0 in the event of a tie. Note that Math.abs(n) returns the absolute value of a number.*/
+
 public class Close10 {
     public static void main(String[] args) {
         System.out.println(close10(9, 11));
