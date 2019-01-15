bad82603bba644ea66cb81b5a66ce1b01ef1ea34
IGNovikov <IGNovikov@esphere.ru>
2019-01-15 10:39:12 +0300

    add Task05

---
+++ src/main/java/CodeGym/Level2/Lesson2/Task05/Solution.java
@@ -0,0 +1,12 @@
+package CodeGym.Level2.Lesson2.Task05;
+
+public class Solution {
+    public static void main(String[] args) {
+        printCircleCircumference(5);
+    }
+
+    public static void printCircleCircumference(int radius) {
+        //write your code here
+        System.out.println(2 * Math.PI * radius);
+    }
+}
