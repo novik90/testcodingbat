f8720107569251da4e3d50eb410c36073d74edbd
IGNovikov <IGNovikov@esphere.ru>
2019-01-15 10:07:23 +0300

    add branch codegym add Task01

---
+++ src/main/java/CodeGym/Level2/Lesson2/Task01.java
@@ -0,0 +1,16 @@
+package CodeGym.Level2.Lesson2;
+
+public class Task01 {
+
+    public static void main(String[] args) {
+        Person person = new Person();
+    }
+
+    private static class Person {
+        String name;
+        int age;
+        int weight;
+        int money;
+    }
+
+}
